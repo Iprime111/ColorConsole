0196f20db0ea4935e4f954c7b735607a45136d6c
Iprime111 <maximatimoshkin@outlook.com>
2023-09-18 22:07:53 +0300

    fprintf_color fixed

--- src/ColorConsole.cpp
+++ src/ColorConsole.cpp
@@ -18,9 +18,9 @@ void fprintf_color (enum CONSOLE_COLOR color, enum CONSOLE_BOLD bold, FILE* stre
     va_list args;
     va_start (args, format);
 
-    printf ("\033[%d;3%dm", bold, color);
+    fprintf (stream, "\033[%d;3%dm", bold, color);
     vfprintf (stream, format, args);
-    printf ("\033[0;39m");
+    fprintf (stream, "\033[0;39m");
 
     va_end (args);
 }
